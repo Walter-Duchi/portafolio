4f39c046db0508318bf6833e93037fd8653c3766
Walter-Duchi <waltduchi@gmail.com>
2025-09-20 15:49:36 -0500

    Add task management components and functionality
    - Implemented app-task-form and app-tasks-page components for task creation and display.
    - Updated app.html to include app-tasks-page.
    - Enhanced tasks-page.html with task input and display logic.
    - Added task management logic in tasks-page.ts.
    - Created task-form.html and task-form.ts for task input handling.
    - Introduced basic styling for task form.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
+import { TasksPage } from './components/tasks-page/tasks-page';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer],
+  imports: [Header, Footer, TasksPage],
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush //evitar comprobaciones innecesarias
--- src/app/components/tasks-page/tasks-page.ts
+++ src/app/components/tasks-page/tasks-page.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { TaskForm } from "../task-form/task-form";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tasks-page',
-  imports: [],
+  imports: [TaskForm, FormsModule],
   templateUrl: './tasks-page.html',
   styleUrl: './tasks-page.css'
 })
 export class TasksPage {
+  tasks: Task[] = [];
 
+  taskName: string = '';
+
+  addTask(taskName: string) {
+    const newTask: Task = {
+      id: this.tasks.length + 1,
+      name: taskName,
+      completed: false
+    };
+    this.tasks.push(newTask);
+    this.taskName = '';
+  }
 }
+
+export interface Task {
+  id: number;
+  name: string;
+  completed: boolean;
+} 
