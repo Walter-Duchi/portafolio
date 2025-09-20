67e373eeb16d300098f935b08c19742754e823f0
Walter-Duchi <waltduchi@gmail.com>
2025-09-20 09:19:25 -0500

    correcion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { Header } from "./components/header/header";
-import { TasksPage } from "./components/tasks-page/tasks-page";
 import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, TasksPage, Footer],
+  imports: [Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  changeDetection: 0 //evitar comprobaciones innecesarias
+  changeDetection: ChangeDetectionStrategy.OnPush //evitar comprobaciones innecesarias
 })
 export class App {
   protected readonly title = signal('portafolio');
