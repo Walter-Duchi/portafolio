11db424f05ba59839aff20afc7ee5ca52f84b4e5
Walter-Duchi <waltduchi@gmail.com>
2025-09-23 10:21:32 -0500

    reestructura inicial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { Header } from "./components/header/header";
-import { Footer } from "./components/footer/footer";
-import { TasksPage } from './components/tasks-page/tasks-page';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, TasksPage],
+  imports: [Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css',
-  changeDetection: ChangeDetectionStrategy.OnPush //evitar comprobaciones innecesarias
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush, //evitar comprobaciones innecesarias
 })
 export class App {
   protected readonly title = signal('portafolio');
